4cdf202595cf81f985a6e97be4973dddc7eb28c2
Potayto <p.o.tayto@outlook.com>
2022-09-10 18:09:50 -0700

    Take player guess and validate it so only letters are included

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -6,5 +6,25 @@ def list_of_possible_words
   end
 end
 
+def guess_has_only_letters?(player_guess)
+  player_guess.match(/^[a-z]+$/)
+end
+
+def validate_player_guess(player_guess)
+  until guess_has_only_letters?(player_guess)
+    puts 'Invalid input. Please enter only letters in your guess.'
+    print 'Enter your guess: '
+    player_guess = gets.chomp.downcase
+  end
+
+  player_guess
+end
+
+def take_player_guess
+  print 'Enter your guess: '
+  validate_player_guess(gets.chomp.downcase)
+end
+
 secret_word = list_of_possible_words.sample
 puts secret_word
+puts "You entered #{take_player_guess}"
